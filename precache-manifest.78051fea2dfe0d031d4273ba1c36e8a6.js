3744d857a41d4618b2e58f16b4d219d7605bece0
alachhman <lachhman.anthony@gmail.com>
2019-08-14 13:07:31 -0400

    Updates

---
+++ precache-manifest.78051fea2dfe0d031d4273ba1c36e8a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553e85c4b7435e9e517eff593d41a596",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "bba80c8c94056536a410",
+    "url": "/page/static/css/main.ba361348.chunk.css"
+  },
+  {
+    "revision": "2b6bd3ca338b3faddf6f",
+    "url": "/page/static/js/2.01815544.chunk.js"
+  },
+  {
+    "revision": "bba80c8c94056536a410",
+    "url": "/page/static/js/main.f7987daf.chunk.js"
+  },
+  {
+    "revision": "52f0d7f9f0fc1aa12f89",
+    "url": "/page/static/js/runtime~main.493d39d8.js"
+  }
+]);
